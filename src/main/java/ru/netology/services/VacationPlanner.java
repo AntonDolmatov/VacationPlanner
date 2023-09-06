7283d9daca6e64743dd481c7f20b74efaf1a99e8
Anton Dolmatov <tosha.dolmatow@yandex.ru>
2023-09-06 19:31:29 +0300

    Правка кода

--- src/main/java/ru/netology/services/VacationPlanner.java
+++ src/main/java/ru/netology/services/VacationPlanner.java
@@ -3,11 +3,11 @@ public class VacationPlanner {
     public int calculateVacationMonths(int income, int expenses, int threshold) {
         int i = 0;
         int money = 0;
-        for (int month = 0; month < 12; month++) {
+        for (int month = 1; month < 12; month++) {
             if (money > threshold) {
                 i++;
                 money = money - expenses;
-                money = money - expenses * 3;
+                money = money - money / 3;
             } else {
                 money = money + income;
                 money = money - expenses;
